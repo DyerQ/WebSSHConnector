38453bbcd6a540f533e677033248050dc3cea1c7
Vasiliy <vurosov@gmail.com>
2016-03-05 19:15:36 +0300

    Add logging

--- WebSSHConnector-ejb/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/beans/MMObjectBean.java
+++ WebSSHConnector-ejb/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/beans/MMObjectBean.java
@@ -1,10 +1,16 @@
 package com.netcracker.edu.bestgroup.projects.ssh.beans;
 
+import org.slf4j.Logger;
+import org.slf4j.LoggerFactory;
+
 import javax.ejb.Stateless;
 
 
 @Stateless(name = "MMObjectEJB")
 public class MMObjectBean {
+    private static final Logger LOGGER = LoggerFactory.getLogger(MMObjectBean.class);
+
     public MMObjectBean() {
+        LOGGER.debug("Hello from MMObject constructor");
     }
 }
