2b64fcb9513e5d71f3e7bd46e9cee72d4f603569
Vasiliy <vurosov@gmail.com>
2016-03-05 18:36:06 +0300

    Upload application AS-IS
    TODO remove unnecessary files

---
+++ WebSSHConnector-ejb/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/beans/MMObjectBean.java
@@ -0,0 +1,10 @@
+package com.netcracker.edu.bestgroup.projects.ssh.beans;
+
+import javax.ejb.Stateless;
+
+
+@Stateless(name = "MMObjectEJB")
+public class MMObjectBean {
+    public MMObjectBean() {
+    }
+}
