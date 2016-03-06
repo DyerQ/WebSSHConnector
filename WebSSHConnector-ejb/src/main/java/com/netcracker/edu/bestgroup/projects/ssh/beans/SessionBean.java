83fb661bac0e4df1f001342c1263b1aebb9e6c90
Vasiliy <vurosov@gmail.com>
2016-03-06 13:53:14 +0300

    Deploy fix, set up variable for manipulation of contextRoot
    While developing, specify variable web-deploy-suffix@application/pom.xml
    to enable deploying of several applications simultaneously

---
+++ WebSSHConnector-ejb/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/beans/SessionBean.java
@@ -0,0 +1,9 @@
+package com.netcracker.edu.bestgroup.projects.ssh.beans;
+
+import javax.ejb.Stateless;
+
+@Stateless(name = "SessionEJB")
+public class SessionBean {
+    public SessionBean() {
+    }
+}
