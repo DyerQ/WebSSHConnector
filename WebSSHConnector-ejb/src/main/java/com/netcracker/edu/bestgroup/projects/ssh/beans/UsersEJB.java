5b538473ef5315ed15ae6970273ca8b7299f9584
MishaTvr <tvere@phystech.edu>
2016-03-13 19:21:25 +0300

    first CRUD commit

--- WebSSHConnector-ejb/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/beans/UsersEJB.java
+++ WebSSHConnector-ejb/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/beans/UsersEJB.java
@@ -1,8 +1,13 @@
 package com.netcracker.edu.bestgroup.projects.ssh.beans;
 
+import com.netcracker.edu.bestgroup.projects.ssh.entities.Users;
+
 import javax.ejb.Stateless;
 import javax.persistence.EntityManager;
 import javax.persistence.PersistenceContext;
+import javax.persistence.TypedQuery;
+import javax.validation.constraints.AssertFalse;
+import java.util.List;
 
 @Stateless(name = "UsersEJB")
 public class UsersEJB {
@@ -11,5 +16,18 @@ public class UsersEJB {
 
     public UsersEJB() {
     }
-
+    public Users addNew(Users user){
+        entityManager.persist(user);
+        return user;
+    }
+    public Users delete(Users user){
+        Users toBeRemoved = entityManager.merge(user);
+        entityManager.remove(toBeRemoved);
+        return user;
+    }
+    public List<Users> findUsers(){
+        TypedQuery<Users> query = (TypedQuery<Users>) entityManager.createNamedQuery("Users.findAll");
+        List<Users> resultList = query.getResultList();
+        return resultList;
+    }
 }
---
+++ WebSSHConnector-web/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/controllers/UsersController.java
@@ -0,0 +1,56 @@
+package com.netcracker.edu.bestgroup.projects.ssh.controllers;
+
+
+import com.netcracker.edu.bestgroup.projects.ssh.beans.UsersEJB;
+import com.netcracker.edu.bestgroup.projects.ssh.entities.Users;
+
+import javax.annotation.PostConstruct;
+import javax.ejb.EJB;
+import javax.faces.bean.ManagedBean;
+import javax.faces.bean.RequestScoped;
+import javax.faces.bean.SessionScoped;
+import java.util.List;
+
+@ManagedBean
+@RequestScoped
+public class UsersController {
+
+    private List<Users> usersList;
+    private Users user = new Users();
+    @EJB
+    private UsersEJB usersEJB;
+
+    @PostConstruct
+    public void postConstruct() {
+        usersList = usersEJB.findUsers();
+    }
+    public String addNewUser(){
+        user = usersEJB.addNew(user);
+        usersList  = usersEJB.findUsers();
+        return "/test/crud.xhtml";
+    }
+    public String editUser(){
+        return null;
+    }
+    public String deleteUser(Users user){
+        usersEJB.delete(user);
+        usersList=usersEJB.findUsers();
+        return "/test/crud.xhtml";
+    }
+
+    public List<Users> getUsersList() {
+        return usersList;
+    }
+
+    public void setUsersList(List<Users> usersList) {
+        this.usersList = usersList;
+    }
+
+    public Users getUser() {
+        return user;
+    }
+
+    public void setUser(Users user) {
+        this.user = user;
+    }
+}
