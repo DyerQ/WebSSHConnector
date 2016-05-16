24515cea28f68492aa775014159172bbbb1e9e5a
DariaChernikova <darya.d.chernikova@gmail.com>
2016-05-16 22:23:48 +0300

    profile

--- WebSSHConnector-web/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/controllers/ProfileController.java
+++ WebSSHConnector-web/src/main/java/com/netcracker/edu/bestgroup/projects/ssh/controllers/ProfileController.java
@@ -10,11 +10,16 @@ import javax.faces.bean.ViewScoped;
 import javax.faces.context.FacesContext;
 import javax.servlet.http.HttpServletRequest;
 import java.io.IOException;
+import java.util.LinkedHashMap;
+import java.util.Map;
 
 @ManagedBean
 @ViewScoped
 public class ProfileController {
     private User currentUser;
+
+    private String avatarSex;
+    private String avatarColor;
 
     @EJB
     private UsersEJB usersEJB;
@@ -39,6 +44,29 @@ public class ProfileController {
         FacesContext.getCurrentInstance().getExternalContext().redirect("main.xhtml");
     }
 
+    public void setAvatar(){
+    //    FacesContext.getCurrentInstance().getExternalContext().
+        Map<String, String> manMap = new LinkedHashMap<>();
+        manMap.put("Blonde", "resources/img/man_blonde.jpg");
+        manMap.put("Ginger", "resources/img/man_ginger.jpg");
+        manMap.put("Brunet", "resources/img/man_brunet.jpg");
+
+        Map<String, String> womanMap = new LinkedHashMap<>();
+        womanMap.put("Blonde", "resources/img/woman_blonde.jpg");
+        womanMap.put("Ginger", "resources/img/woman_ginger.jpg");
+        womanMap.put("Brunet", "resources/img/woman_brunet.jpg");
+
+        Map<String, Map<String, String>> outerMap = new LinkedHashMap<>();
+        outerMap.put("Man", manMap);
+        outerMap.put("Woman", womanMap);
+
+        if (avatarSex == null || avatarSex.isEmpty() || avatarColor == null || avatarColor.isEmpty()) {
+            currentUser.setAvatar("resources/img/standart.jpg");
+        } else {
+            currentUser.setAvatar(outerMap.get(avatarSex).get(avatarColor));
+        }
+    }
+
 
     public User getCurrentUser() {
         return currentUser;
@@ -46,5 +74,21 @@ public class ProfileController {
 
     public void setCurrentUser(User currentUser) {
         this.currentUser = currentUser;
+    }
+
+    public String getAvatarSex() {
+        return avatarSex;
+    }
+
+    public void setAvatarSex(String avatarSex) {
+        this.avatarSex = avatarSex;
+    }
+
+    public String getAvatarColor() {
+        return avatarColor;
+    }
+
+    public void setAvatarColor(String avatarColor) {
+        this.avatarColor = avatarColor;
     }
 }
